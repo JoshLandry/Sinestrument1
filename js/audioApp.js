ea778e772131696b5207e81af7c4ec967ea5ad8a
Josh <bluemazaro@yahoo.com>
2015-06-19 02:17:16 -0700

    initial commit, working sine generator and mute button

---
+++ js/audioApp.js
@@ -0,0 +1,40 @@
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+var oscillator = audioCtx.createOscillator();
+var gainNode = audioCtx.createGain();
+
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+
+//
+
+var initialVol = 0.010;
+
+//
+
+oscillator.type = 'sine';
+oscillator.frequency.value = 4400;
+oscillator.detune.value = 100;
+oscillator.start(0);
+
+oscillator.onended = function() {
+  console.log('somehow, your tone has ended');
+}
+
+gainNode.gain.value = initialVol;
+
+// mute button
+
+var mute = document.querySelector('.mute');
+
+mute.onclick = function() {
+  if(mute.getAttribute('data-muted') === 'false') {
+    gainNode.disconnect(audioCtx.destination);
+    mute.setAttribute('data-muted', 'true');
+    mute.innerHTML = "Unmute";
+  } else {
+    gainNode.connect(audioCtx.destination);
+    mute.setAttribute('data-muted', 'false');
+    mute.innerHTML = "Mute";
+  };
+}
