589b74f35c045e63ee6ad454e02b1fd0d4481ad3
Josh <bluemazaro@yahoo.com>
2016-03-26 23:37:14 -0700

    server test is functional, cannot proceed with synth app testing yet because of inadequate means for mocking Web Audio API elements

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('../index.js');
+require('../views/js/audioApp.js');
+
 var chai = require('chai');
 var chaihttp = require('chai-http');
 var fs = require('fs');
@@ -9,14 +11,24 @@ chai.use(chaihttp);
 
 var expect = chai.expect;
 
-describe('', function() {
- it('should GET index', function(done) {
-   chai.request('localhost:5000')
-     .get('/')
-     .end(function(err, res) {
-       expect(err).to.eql(null);
-       expect(res).to.have.status(200);
-       done();
-     });
- });
+// Server Tests
+
+describe('Express Server', function() {
+  it('should GET index', function(done) {
+     chai.request('localhost:5000')
+       .get('/')
+       .end(function(err, res) {
+         expect(err).to.eql(null);
+         expect(res).to.have.status(200);
+         done();
+       });
+  });
+});
+
+// Synth App Tests
+
+describe('Synth App', function() {
+  it('should create filter', function(done) {
+    // must find some way to mock Web Audio API elements.
+  });
 });
